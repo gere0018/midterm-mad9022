f2286da0cc7e46cfca478a00a4aa54c386b76453
gere0018 <gere0018@algonquinlive.com>
2015-03-26 19:50:31 -0400

    made significant changes to code. completely changed the sequence and removed redundancy in creating maps multiple times. also adjusted styles to better fit orientation mode.

--- platforms/android/assets/www/js/script.js
+++ platforms/android/assets/www/js/script.js
@@ -3,10 +3,9 @@ var midterm_gere0018 = {
     loadCount: 0,
     listview: [],
     pages:[],
+    map:"",
     contacts:"",
     contactPostion:"",
-    contactLatitude:"",
-    contactLongitude:"",
     contactsMap: "",
     backBtn: "",
     overlay:"",
@@ -38,8 +37,6 @@ var midterm_gere0018 = {
         H1 = document.querySelector("h1");
         contactsMap = document.querySelector("#contactsMap");
 
-
-
         //find the user's location to create map*****************************************
         if( navigator.geolocation ){
         var getLocation = {enableHighAccuracy: false, timeout:60000, maximumAge:60000};
@@ -59,8 +56,8 @@ var midterm_gere0018 = {
 
         //add hammer Listener to the Ul listview*****************************************
         var hammerlistview = new Hammer.Manager(listview);
-        var singleTap = new Hammer.Tap({ event: 'singletap' });
-        var doubleTap = new Hammer.Tap({event: 'doubletap', taps: 2 });
+        var singleTap = new Hammer.Tap({ event: 'singletap', threshold: 10 });
+        var doubleTap = new Hammer.Tap({event: 'doubletap', taps: 2, threshold: 15, posThreshold: 30 });
         hammerlistview.add([doubleTap, singleTap]);
         doubleTap.recognizeWith(singleTap);
         singleTap.requireFailure([doubleTap]);
@@ -73,8 +70,41 @@ var midterm_gere0018 = {
       //Geolocation success function
     reportPosition:function( position ){
         contactPostion = position;
+         //create the map with center current user position
+        var mapOptions ={
+          center:new google.maps.LatLng(contactPostion.coords.latitude,
+                                        contactPostion.coords.longitude),
+          zoom:14,
+          disableDoubleClickZoom: true,
+          zoomControl: false,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map =new google.maps.Map(contactsMap, mapOptions);
+        midterm_gere0018.marker = new google.maps.Marker({
+                                                      animation: google.maps.Animation.BOUNCE,
+                                                      map: map
+                                                    });
 
    },
+     //placemarker function allows the user only one marker
+   placeMarker: function placeMarker(location) {
+        console.log("Inside placeMarker");
+//                  if ( midterm_gere0018.marker ) {
+//                      console.log("marker has a value");
+//                    midterm_gere0018.marker.setPosition(location);
+//
+//                   // midterm_gere0018.marker.setAnimation(null);
+//                    midterm_gere0018.marker.setAnimation(google.maps.Animation.DROP);
+//                  } else {
+//                      console.log("marker new");
+          midterm_gere0018.marker.setPosition(location);
+          midterm_gere0018.marker.setAnimation(google.maps.Animation.BOUNCE);
+          midterm_gere0018.marker.setMap(map);
+
+    },
+
+
+
     //Contacts success function
     contactsSuccess: function (deviceContacts){
         contacts = deviceContacts;
@@ -83,8 +113,9 @@ var midterm_gere0018 = {
             console.log("window has localstorage");
              parsedContacts = JSON.parse(localStorage.getItem("myContacts"));
             //when first opening app check if key doesn't exists in local storgae
-            if (parsedContacts === null) {
+            if (parsedContacts == null) {
                 parsedContacts = [];
+                console.log("this is: " + parsedContacts);
             }
         }
          //loop through my loaded contacts and transform each into a JSOn object
@@ -95,6 +126,7 @@ var midterm_gere0018 = {
                  "name": contacts[i].displayName,
                  "numbers": contacts[i].phoneNumbers,
              };
+            parsedContacts.push(objectContact);
             //if there is a saved lat and long value from previous use. when app first opens,
             //add values it to my contact object. otherwise lat and lng are set to null.
             if( parsedContacts[i].lat){
@@ -108,7 +140,7 @@ var midterm_gere0018 = {
                 objectContact.lng = null;
             }
 
-             parsedContacts.push(objectContact);
+
         }
         //stringify the parsedContacts and save it in local storage as a value for key myContacts.
          var stringContacts = JSON.stringify(parsedContacts);
@@ -154,52 +186,58 @@ var midterm_gere0018 = {
    },
     //function that gets executed on single tap
     displayContactLocation: function(ev){
+         //get the data-ref value of the li that is clicked to get its contact info
+         var i = ev.target.getAttribute("data-ref");
         //clear all existing markers
-        console.log(midterm_gere0018.marker);
          if(midterm_gere0018.marker){
              midterm_gere0018.marker.setMap(null);
              console.log(" clear marker");
-             console.log(midterm_gere0018.marker);
          }
-        //get the data-ref value of the li that is clicked to get its contact info
-         var i = ev.target.getAttribute("data-ref");
+         //display the location page containing the map
+          pages[0].classList.remove("activePage");
+          pages[1].classList.add("activePage");
+          //push history state to allow hardware backbtn to function
+          history.pushState(null, null, "#location");
+          // resizing the map after setting the location page display to block.
+          //without this code map doesn't show fully.only small portion shows
+          google.maps.event.trigger(map, "resize");
+          //map.setZoom(map.getZoom());
+          //adjust title position to accomodate backbtn
+          H1.classList.add("movedH1");
+          //display backbtn
+          backBtn.style.display = "inline-block";
+          //add hammer on back button. I noticed that hammer works only after button's diplay is
+          //block. hammer didn't work when added before.
+          var hammerBackBtn = new Hammer( backBtn);
+          hammerBackBtn.on("tap", midterm_gere0018.browserBackButton);
+         //add an event listener on my map. when double clicked add marker
+         //and save position in localstorage.
+         google.maps.event.addListener(map, 'dblclick', function(event) {
+                console.log("setting marker");
+                midterm_gere0018.placeMarker(event.latLng);//console.log(event.latLng);
+
+                //set lat and long in the parsedcontacts object
+                parsedContacts[i].lat = event.latLng.k;
+                parsedContacts[i].lng = event.latLng.D;
+
+                 console.log(parsedContacts[i]);
+                //reset the whole object in local storage everytime you add a new
+                //value or make a change. Cannot add the changed value alone.
+                 localStorage.setItem("myContacts", JSON.stringify(parsedContacts));
+
+            });
+
         //check if this contact's lat and lng have been set in local storage
          if(parsedContacts[i].lat && parsedContacts[i].lng){
              console.log("lat and long has saved value");
-             //display the location page containing the map
-              pages[0].classList.remove("activePage");
-              pages[1].classList.add("activePage");
-              //push history state to allow hardware backbtn to function
-              history.pushState(null, null, "#location");
-              //adjust title position to accomodate backbtn
-              H1.classList.add("movedH1");
-              //display backbtn
-              backBtn.style.display = "inline-block";
+              //set map with existing lat and long values.
+              var latlng = new google.maps.LatLng(parsedContacts[i].lat, parsedContacts[i].lng);
 
-         //add hammer on back button. I noticed that hammer works only after button's diplay is
-         //block. hammer didn't work when added before.
-          var hammerBackBtn = new Hammer( backBtn);
-          hammerBackBtn.on("tap", midterm_gere0018.browserBackButton);
+              midterm_gere0018.marker.setPosition(latlng);
+              midterm_gere0018.marker.setAnimation(google.maps.Animation.BOUNCE);
+              midterm_gere0018.marker.setMap(map);
 
-
-        //set map with existing lat and long values.
-          var myLatlng = new google.maps.LatLng(parsedContacts[i].lat,
-                                                   parsedContacts[i].lng);
-          var mapOptions ={
-              center:myLatlng,
-              zoom:14,
-              zoomControl: false,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-          var map =new google.maps.Map(contactsMap, mapOptions);
-          //add animated marker in lat and long position
-          midterm_gere0018.marker = new google.maps.Marker({
-              position: myLatlng,
-              map: map,
-              animation: google.maps.Animation.DROP,
-              title: 'user position'
-          });
-
+              map.setCenter(latlng);
         }else{
              //if there is no saved value for longitude and latitude in local storage,
             //display msg to user
@@ -212,66 +250,8 @@ var midterm_gere0018 = {
             hammerOk2.on('tap', function(){
                 msgBox.style.display = "none";
                 overlay.style.display = "none";
-                pages[0].classList.remove("activePage");
-                pages[1].classList.add("activePage");
-
-                history.pushState(null, null, "#location");
-                H1.classList.add("movedH1");
-                backBtn.style.display = "inline-block";
-                //add hammer on back button. I noticed that hammer works only after button's diplay is
-                //block. hammer didn't work when added before. that's way this code is redundant in
-                //if and else.
-                var hammerBackBtn = new Hammer( backBtn);
-                hammerBackBtn.on("tap", midterm_gere0018.browserBackButton);
-
-                //create the map with center current user position
-                var mapOptions ={
-                  center:new google.maps.LatLng(contactPostion.coords.latitude,
-                                                contactPostion.coords.longitude),
-                  zoom:14,
-                  disableDoubleClickZoom: true,
-                  zoomControl: false,
-                  mapTypeId: google.maps.MapTypeId.ROADMAP
-                };
-                var map =new google.maps.Map(contactsMap, mapOptions);
-
-                //placemarker function allows the user only one marker
-                function placeMarker(location) {
-                    console.log("Inside placeMarker");
-                  if ( midterm_gere0018.marker ) {
-                      console.log("marker has a value");
-                    midterm_gere0018.marker.setPosition(location);
-                    midterm_gere0018.marker.setMap(map);
-                    midterm_gere0018.marker.setAnimation(null);
-                    midterm_gere0018.marker.setAnimation(google.maps.Animation.DROP);
-                  } else {
-                    midterm_gere0018.marker = new google.maps.Marker({
-                      position: location,
-                      animation: google.maps.Animation.DROP,
-                      //draggable:true,
-                      map: map
-                    });
-                  }
-                }
-
-                google.maps.event.addListener(map, 'dblclick', function(event) {
-                    console.log("setting marker");
-                    placeMarker(event.latLng);//console.log(event.latLng);
-
-                    //set lat and long in the parsedcontacts object
-                    parsedContacts[i].lat = event.latLng.k;
-                    parsedContacts[i].lng = event.latLng.D;
-
-                     console.log(parsedContacts[i]);
-                    //reset the whole object in local storage everytime you add a new
-                    //value or make a change. Cannot add the changed value alone.
-                     localStorage.setItem("myContacts", JSON.stringify(parsedContacts));
-
-                });
-
             });
         }
-
 
     },
     browserBackButton:function (ev){
@@ -289,6 +269,17 @@ var midterm_gere0018 = {
         2: 'Position unavailable',
         3: 'Request timeout'
       };
+        contactPostion = position;
+         //create the map with center current user position
+        var mapOptions ={
+          center:new google.maps.LatLng(0,0),
+          zoom:14,
+          disableDoubleClickZoom: true,
+          zoomControl: false,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map =new google.maps.Map(contactsMap, mapOptions);
+
     //in case of erros the following function gives an explanation of type of error to the user.
       alert("Error: " + errors[error.code]);
     },
